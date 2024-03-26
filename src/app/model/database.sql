83b6ba80644eaaef2e28eb9b9700318d187cfe6b
Federico crespi <fdcrespi@gmail.com>
2024-03-26 10:37:04 -0300

    Update database.sql

--- src/app/model/database.sql
+++ src/app/model/database.sql
@@ -15,17 +15,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   CONSTRAINT `fk_user_role` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `user_supplies` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
-  `id_sent`int(11) NOT NULL DEFAULT '1',
-  `supplie` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_user_profile_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 INSERT INTO `role` (`id`, `name`) VALUES
 (1, 'user'),
 (2, 'admin'),
